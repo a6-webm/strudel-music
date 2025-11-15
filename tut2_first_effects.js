d8edff60a1b96bca6795e5f5e8c30f799c5a7d21
a6.webm <46289515+a6-webm@users.noreply.github.com>
2025-11-15 10:57:52 +0000

    tut2

--- tut2_first_effects.js
+++ tut2_first_effects.js
@@ -1,12 +1,23 @@
 setcpm(100/4)
-$: s(`[- - hh hh] [- - - hh] [- - hh -] <[- - hh -] [- hh hh hh]>`).bank("casiorz1").gain("[1 0.2 1 0.2]*4")
-$: s("[bd - - -] [<- - sd sd> - - -] [bd - - -] [- - - -]").bank("casiorz1")
-  
-$: note(`<
-c4  c4  e4  e4,
-bb4 bb4 a4  e4,
-a5  g5  e5  f5
->`).s("saw")
-.lpf(1000)
-  .adsr("0.5:0:1:0.1")
+$: s(`[- - hh hh] [<- - sd -> - - hh] [- - hh -] <[- - hh -] [- hh hh hh]>`)
+  .bank("casiorz1")
+  .gain("[1 0.2 1 0.2]*4")
+  .room(0.05)
+
+$: s("pink")
+  .lpf(3000)
+  .adsr("3:0:1:3")
+  .fast(rand.range(1, 2))
   .gain(0.3)
+
+$: note(`
+<c4*4  g3*2    c3*4  c4*2>,
+<bb4*4 [c5 b4] bb4*4 f4*2>,
+<a5*4  [a5 g5] f5*4  e5*2>
+`).s("saw")
+  .vowel("<[o e a o] [o e] [u o e o] [e i]>")
+  .att("<0.2 0.1>")
+  .dec("<0.1 0.3>")
+  .sus(0)
+  .rel(0)
+  .room(0.2)
